89f6f3daae29805c14ca1bb3bfab6196edb4a15e
Ansh Sisodiya <mranshsisodiya@gmail.com>
2026-06-04 20:45:24 +0530

    basics of array for starting of the questions

--- Arrays/ArraysBasic.java
+++ Arrays/ArraysBasic.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class ArraysBasic{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Scanner sc= new Scanner(System.in); //declare a sc to scan the numbers or characters given in question
+        int arr[]=new int[5];  //declare array
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);  //iterate the array to check what values the system have assigned
+        }
+        //in this main method, the max size of array can be 10^6
+        //but globally the max size of array will be 10^7
+        
     }
 }
